e94776939bdb8ac6af94cbfefeea0851f0f4bff6
yiminghe <yiminghe@gmail.com>
2010-07-27 05:49:50 +0000

    no-copy experimental

---
+++ lite-ext/playground/no-copy/content.js
@@ -0,0 +1,36 @@
+KISSY.ready(function(S) {
+    var t = "textarea",
+        Event = S.Event,
+        doc = document,
+        body = doc.body,
+        DOM = S.DOM,
+        Node=S.Node,
+        clip = new Node(body.appendChild(DOM.create('<' + t + ' rows="1" cols="1" id="clipboard" style="position:absolute;left:-9999px;"></' + t + '>')))
+
+    Event.on(document, "contextmenu", function(ev) {
+        ev.halt();
+    });
+    Event.on(document, "keydown", function(ev) {
+        var keyCode = ev.keyCode;
+
+        //mac and win
+        //捕获复制快捷键
+        if (keyCode === 67 && (ev.ctrlKey || ev.metaKey)) {
+            //没有警告信息这个就够了
+            //ev.halt();
+            //return;
+
+            //保存原来的选择区域
+            var s = new S.Selection(doc),r = s.getRanges()[0];
+            //警告
+            clip.html("禁止copy！");
+            clip[0].focus();
+            clip[0].select();
+            //异步
+            setTimeout(function() {
+                r.select();
+            }, 0);
+        }
+    });
+    body.appendChild(document.createTextNode("我是内容，你可以copy我看看！"));
+});
