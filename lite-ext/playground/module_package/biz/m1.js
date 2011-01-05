2f8728ecc6cdeb50dcd314e5b2e3de3296b91f6f
yiminghe <yiminghe@gmail.com>
2011-01-05 05:04:17 +0000

    module package

---
+++ lite-ext/playground/module_package/biz/m1.js
@@ -0,0 +1,9 @@
+﻿KISSY.add("biz/m1",function(S,DOM,Event,m2){
+	var btn=DOM.create("<button>模块1初始化了，可以按我</button>");
+	DOM.prepend(btn,document.body);
+	Event.on(btn,"click",function(){
+			m2.log("模块3调用模块2！");
+	});
+},{
+	requires:["dom","event","biz/m1/m2"]
+});
---
+++ lite-ext/playground/module_package/biz/m1/m2.js
@@ -0,0 +1,11 @@
+﻿KISSY.add("biz/m1/m2",function(S,DOM){
+	var info=DOM.create("<div style='font-size:30px;'>模块2初始化！</div>");
+	DOM.prepend(info,document.body);
+	return {
+			log:function(s){
+				DOM.html(info,s);
+			}
+	};
+},{
+	requires:["dom"]
+});
