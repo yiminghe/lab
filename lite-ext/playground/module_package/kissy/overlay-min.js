2730e386a0f119028ef2c4d607144d240dd1a14a
yiminghe <yiminghe@gmail.com>
2011-05-05 09:10:19 +0000

    update module demo

--- lite-ext/playground/module_package/kissy/overlay-min.js
+++
@@ -1,7 +0,0 @@
-/*
-Copyright 2011, KISSY UI Library v1.1.7dev
-MIT Licensed
-build time: ${build.time}
-*/
-KISSY.add("overlay/overlay",function(d,b,c){function a(e){return d.require("uibase/"+e)}return c.create([a("box"),a("contentbox"),a("position"),a("loading"),b.ie==6?a("shim"):null,a("align"),a("resize"),a("mask")],{initializer:function(){},renderUI:function(){this.get("el").addClass("ks-overlay")},syncUI:function(){},bindUI:function(){},destructor:function(){}},{ATTRS:{elOrder:0}})},{requires:["ua","uibase"]});
-KISSY.add("overlay/dialog",function(d,b,c){function a(e){return d.require("uibase/"+e)}return c.create(b,[a("stdmod"),a("close"),a("drag"),a("constrain")],{initializer:function(){},renderUI:function(){this.get("el").addClass("ks-dialog");this.set("handlers",[this.get("header")])},bindUI:function(){},syncUI:function(){},destructor:function(){}})},{requires:["overlay/overlay","uibase"]});KISSY.add("overlay",function(d,b,c){b.Dialog=c;return b},{requires:["overlay/overlay","overlay/dialog"]});
--- lite-ext/playground/module_package/kissy/template-min.js
+++
@@ -1,6 +0,0 @@
-/*
-Copyright 2011, KISSY UI Library v1.1.7dev
-MIT Licensed
-build time: ${build.time}
-*/
-
