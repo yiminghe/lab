c222f6a11bcea76cc85a19a41425c68c381404b7
yiminghe <yiminghe@gmail.com>
2024-07-15 13:32:39 +0800

    utils

---
+++ playground/get-unpublished-packages/index.js
@@ -0,0 +1,119 @@
+const { getPackages } = require('@manypkg/get-packages');
+const exec = require('@yiminghe/shell-exec');
+
+function getCorrectRegistry(packageJson) {
+  const registry =
+    packageJson?.publishConfig?.registry ?? process.env.npm_config_registry;
+
+  return !registry || registry === 'https://registry.yarnpkg.com'
+    ? 'https://registry.npmjs.org'
+    : registry;
+}
+
+async function getPackageInfo(packageJson) {
+  console.info(`npm info ${packageJson.name}`);
+
+  // Due to a couple of issues with yarnpkg, we also want to override the npm registry when doing
+  // npm info.
+  // Issues: We sometimes get back cached responses, i.e old data about packages which causes
+  // `publish` to behave incorrectly. It can also cause issues when publishing private packages
+  // as they will always give a 404, which will tell `publish` to always try to publish.
+  // See: https://github.com/yarnpkg/yarn/issues/2935#issuecomment-355292633
+  let result = exec(
+    [
+      'npm',
+      'info',
+      packageJson.name,
+      '--registry',
+      getCorrectRegistry(packageJson),
+      '--json',
+    ].join(' '),
+  );
+
+  // Github package registry returns empty string when calling npm info
+  // for a non-existent package instead of a E404
+  if (result.stdout.toString() === '') {
+    return {
+      error: {
+        code: 'E404',
+      },
+    };
+  }
+  return JSON.parse(result.stdout.toString());
+}
+
+async function infoAllow404(packageJson) {
+  let pkgInfo = await getPackageInfo(packageJson);
+  if (pkgInfo.error?.code === 'E404') {
+    console.warn(`Received 404 for npm info ${`"${packageJson.name}"`}`);
+    return { published: false, pkgInfo: {} };
+  }
+  if (pkgInfo.error) {
+    error(
+      `Received an unknown error code: ${
+        pkgInfo.error.code
+      } for npm info ${`"${packageJson.name}"`}`,
+    );
+    error(pkgInfo.error.summary);
+    if (pkgInfo.error.detail) error(pkgInfo.error.detail);
+
+    throw new ExitError(1);
+  }
+  return { published: true, pkgInfo };
+}
+
+async function getUnpublishedPackages(packages) {
+  const results = await Promise.all(
+    packages.map(async ({ packageJson }) => {
+      const response = await infoAllow404(packageJson);
+      let publishedState = 'never';
+      if (response.published) {
+        publishedState = 'published';
+      }
+
+      return {
+        name: packageJson.name,
+        localVersion: packageJson.version,
+        publishedState,
+        publishedVersions: response.pkgInfo.versions || [],
+      };
+    }),
+  );
+
+  const packagesToPublish = [];
+
+  for (const pkgInfo of results) {
+    const { name, publishedState, localVersion, publishedVersions } = pkgInfo;
+    if (!publishedVersions.includes(localVersion)) {
+      packagesToPublish.push(pkgInfo);
+      console.info(
+        `${name} is being published because our local version (${localVersion}) has not been published on npm`,
+      );
+    } else {
+      // If the local version is behind npm, something is wrong, we console.warn here, and by not getting published later, it will fail
+      console.warn(
+        `${name} is not being published because version ${localVersion} is already published on npm`,
+      );
+    }
+  }
+
+  return packagesToPublish;
+}
+
+module.exports = async () => {
+  const { packages, tool } = await getPackages(process.cwd());
+
+  const publicPackages = packages.filter((pkg) => !pkg.packageJson.private);
+
+  const unpublishedPackagesInfo = await getUnpublishedPackages(publicPackages);
+
+  return unpublishedPackagesInfo;
+};
+
+if (require.main === module) {
+  (async function () {
+    console.log(getCorrectRegistry());
+    const unpublishedPackagesInfo = await module.exports();
+    console.log(unpublishedPackagesInfo);
+  })();
+}
---
+++ playground/shell-exec/tempdir.js
@@ -0,0 +1,100 @@
+var os = require('os');
+var fs = require('fs');
+function statFollowLinks() {
+  return fs.statSync.apply(fs, arguments);
+}
+function randomFileName() {
+  function randomHash(count) {
+    if (count === 1) {
+      return parseInt(16 * Math.random(), 10).toString(16);
+    }
+    var hash = '';
+    for (var i = 0; i < count; i++) {
+      hash += randomHash(1);
+    }
+    return hash;
+  }
+
+  return 'shelljs_' + randomHash(20);
+}
+function unlinkSync(file) {
+  try {
+    fs.unlinkSync(file);
+  } catch (e) {
+    // Try to override file permission
+    /* istanbul ignore next */
+    if (e.code === 'EPERM') {
+      fs.chmodSync(file, '0666');
+      fs.unlinkSync(file);
+    } else {
+      throw e;
+    }
+  }
+}
+// Returns false if 'dir' is not a writeable directory, 'dir' otherwise
+function writeableDir(dir) {
+  if (!dir || !fs.existsSync(dir)) return false;
+
+  if (!statFollowLinks(dir).isDirectory()) return false;
+
+  var testFile = dir + '/' + randomFileName();
+  try {
+    fs.writeFileSync(testFile, ' ');
+    unlinkSync(testFile);
+    return dir;
+  } catch (e) {
+    /* istanbul ignore next */
+    return false;
+  }
+}
+
+// Variable to cache the tempdir value for successive lookups.
+var cachedTempDir;
+
+//@
+//@ ### tempdir()
+//@
+//@ Examples:
+//@
+//@ ```javascript
+//@ var tmp = tempdir(); // "/tmp" for most *nix platforms
+//@ ```
+//@
+//@ Searches and returns string containing a writeable, platform-dependent temporary directory.
+//@ Follows Python's [tempfile algorithm](http://docs.python.org/library/tempfile.html#tempfile.tempdir).
+function _tempDir() {
+  if (cachedTempDir) return cachedTempDir;
+
+  cachedTempDir =
+    writeableDir(os.tmpdir()) ||
+    writeableDir(process.env.TMPDIR) ||
+    writeableDir(process.env.TEMP) ||
+    writeableDir(process.env.TMP) ||
+    writeableDir(process.env.Wimp$ScrapDir) || // RiscOS
+    writeableDir('C:\\TEMP') || // Windows
+    writeableDir('C:\\TMP') || // Windows
+    writeableDir('\\TEMP') || // Windows
+    writeableDir('\\TMP') || // Windows
+    writeableDir('/tmp') ||
+    writeableDir('/var/tmp') ||
+    writeableDir('/usr/tmp') ||
+    writeableDir('.'); // last resort
+
+  return cachedTempDir;
+}
+
+// Indicates if the tempdir value is currently cached. This is exposed for tests
+// only. The return value should only be tested for truthiness.
+function isCached() {
+  return cachedTempDir;
+}
+
+// Clears the cached tempDir value, if one is cached. This is exposed for tests
+// only.
+function clearCache() {
+  cachedTempDir = undefined;
+}
+
+module.exports.tempDir = _tempDir;
+module.exports.isCached = isCached;
+module.exports.clearCache = clearCache;
