2ffa9ca7b3f0b433e9aa04fc3269763d02c0675c
yiminghe <yiminghe@gmail.com>
2026-01-24 01:32:15 +0800

    pm2 lock install

--- playground/lock-install-pm2/uninstall.sh
+++ playground/lock-install-pm2/uninstall.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+export PATH="$HOME/pm2/node_modules/.bin:$PATH"
+
 if [ -f ~/pm2/node_modules/.bin/pm2 ]; then
     # 卸载 PM2 模块
-    ~/pm2/node_modules/.bin/pm2 uninstall pm2-logrotate
-    ~/pm2/node_modules/.bin/pm2 uninstall pm2-prom-module
+    pm2 uninstall pm2-logrotate
+    pm2 uninstall pm2-prom-module
     # 停止并删除所有应用
-    ~/pm2/node_modules/.bin/pm2 delete all
+    pm2 delete all
     # 停止 PM2 守护进程
-    ~/pm2/node_modules/.bin/pm2 kill
+    pm2 kill
 fi
 
 # 删除 PM2 相关目录
